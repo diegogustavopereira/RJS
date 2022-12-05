1f2261081a049565176045ee3a1b3669478ed675
Diego Gustavo Pereira <diegogustavopereira@gmail.com>
2022-12-05 20:19:39 -0300

    home modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
-<<<<<<< HEAD
-import Home from './pages/home/home';
-=======
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
->>>>>>> 33bf7711d5c58a1fa1d78e4fac730b7db6a940cd
+import Home from './pages/home/home';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Home/>
-=======
-      <Routes>
+       <Routes>
         <Route path="/login" element={ <Login /> } />
         <Route path="/cadastro" element={ <Register /> } />
+        <Route path='/' element={ <Home /> } />
       </Routes>
->>>>>>> 33bf7711d5c58a1fa1d78e4fac730b7db6a940cd
     </div>
   );
 }
