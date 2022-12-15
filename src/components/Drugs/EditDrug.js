706033b1145d6bae2c9bd52e780b90d41f5e0c57
Paula Bittencourt <paula.bittencourt@gmail.com>
2022-12-15 11:04:19 -0300

    adicionar medicamento

---
+++ src/components/Drugs/EditDrug.js
@@ -0,0 +1,87 @@
+import { useEffect } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { api } from "../../api/api.js";
+
+function EditDrug({ id, drugForm, setDrugForm }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchDrug = async () => {
+      const response = await api.get(`/drug/${id}`);
+      setDrugForm(response.data);
+    };
+
+    fetchDrug();
+  }, [id, setDrugForm]);
+
+  const handleChange = (e) => {
+    setDrugForm({ ...drugForm, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await api.put(`drug/edit/${id}`, drugForm);
+      navigate("/drug");
+
+      toast.success("Dados do medicamento alterados!", {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Nome do Medicamento</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Insira o nome do medicamento"
+            name="name"
+            value={drugForm.name}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>CID da doença</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Insira o nome da doença"
+            name="CID"
+            value={drugForm.CID}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Button
+          className="mt-4"
+          variant="success"
+          type="submit"
+          style={{ margin: "5px" }}
+        >
+          Atualizar medicamento
+        </Button>
+
+        <Button className="mt-4" variant="info" style={{ margin: "5px" }}>
+          <Link to={"/drug"}>Voltar</Link>
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default EditDrug;
